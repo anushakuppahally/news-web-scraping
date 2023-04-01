fbc1fd1486312ceef4395d417c33d1686e4b228f
anushakuppahally <aak154@georgetown.edu>
2023-04-01 17:38:29 -0400

    Added sample html file

--- app/job.py
+++ app/job.py
@@ -13,6 +13,20 @@ load_dotenv()
 api_key = os.getenv("API_KEY")
 
 nyt = NYTAPI(api_key, parse_dates=True)
+headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+
+def test_html():
+    '''
+    Creates file with HTML output of a sample article
+    '''
+    link = "https://www.nytimes.com/2023/01/14/business/media/tv-historical-dramas-fictional.html"
+    response=requests.get(link, headers=headers)
+    soup = BeautifulSoup(response.text, "html.parser")
+    file = open("html.txt", "w")
+    soup_html = repr(soup.prettify())
+    file.write(soup_html)
+    file.close
+
 
 def search_articles():
     '''
@@ -28,7 +42,6 @@ def scrape_articles(article_urls):
     '''
     Scrapes each URL and returns full text of article 
     '''
-    headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
     df = pd.DataFrame()
     text = []
     for link in article_urls:
